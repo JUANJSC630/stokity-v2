faaa312cfb9b6c1d9e9967d2b8f4e3a88b01c98d
Juan Saldarriaga <juan@ocmiwc.com>
2025-07-08 12:48:46 -0400

    fix: reorder branch routes for correct trash management handling

--- routes/branches.php
+++ routes/branches.php
@@ -7,14 +7,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth', \App\Http\Middleware\AdminMiddleware::class])->group(function () {
     // Regular branch CRUD routes
     Route::get('/branches', [BranchController::class, 'index'])->name('branches.index');
-    Route::get('/branches/create', [BranchController::class, 'create'])->name('branches.create');
-    Route::post('/branches', [BranchController::class, 'store'])->name('branches.store');
-    Route::get('/branches/{branch}', [BranchController::class, 'show'])->name('branches.show');
     
-    // Trash management routes - these need to come before wildcard routes to avoid conflicts
+    // Trash management route - must come before other specific routes to avoid conflicts with {branch} parameter
     Route::get('/branches/trashed', [BranchController::class, 'trashed'])->name('branches.trashed');
     
+    // Creation routes
+    Route::get('/branches/create', [BranchController::class, 'create'])->name('branches.create');
+    Route::post('/branches', [BranchController::class, 'store'])->name('branches.store');
+    
     // Routes with dynamic parameters
+    Route::get('/branches/{branch}', [BranchController::class, 'show'])->name('branches.show');
     Route::get('/branches/{branch}/edit', [BranchController::class, 'edit'])->name('branches.edit');
     Route::put('/branches/{branch}', [BranchController::class, 'update'])->name('branches.update');
     Route::delete('/branches/{branch}', [BranchController::class, 'destroy'])->name('branches.destroy');
